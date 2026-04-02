6261bf8172098fc685729fe8a849c3005cb546ce
dinesh13ad <720822108016@hit.edu.in>
2026-04-02 23:02:27 +0530

    difference between sum if even and odd

---
+++ evenandodd.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class evenandodd{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt();
+        System.out.print(sum(n));
+    }
+
+    public static int sum(int n){
+        int ans=0;
+        int sum=0;
+        while(n>0)
+        {
+            int m=n%10;
+            if(m%2==0)
+            {
+                ans = ans + m;
+            }
+            else{
+                sum=sum+m;
+            }
+            n=n/10;
+
+        }
+        return (ans-sum);
+    }
+}
