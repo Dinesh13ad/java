7f2675853e351b70a341a2f90e105dee0cc92531
dinesh13ad <720822108016@hit.edu.in>
2026-04-04 11:22:23 +0530

    Deficient no

---
+++ deficient.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class deficient{
+
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter a number :");
+        int n=sc.nextInt();
+        System.out.print(abundant(n)? "Abundant number" : "Not abundant number");
+    }
+
+    public static boolean abundant(int n){
+        int sum =0;
+        for(int i=1;i<=n/2;i++)
+        {
+            if(n%i==0)
+            {
+                sum=sum+i;
+            }
+        }
+        return (sum<n);
+    }
+}
