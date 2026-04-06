8c96153d2d432b4cfd572de7be261dd2485a8e94
dinesh13ad <720822108016@hit.edu.in>
2026-04-06 22:14:06 +0530

    To find power of 2

--- powerof2.java
+++ powerof2.java
@@ -8,9 +8,14 @@ public class powerof2{
         System.out.print(power(n)? "yes":"no");
     }
     public static boolean power(int n)
-    {
-        if(n%2==0)
-        return true;
+    {    
+        if(n<=0)
         return false;
+
+        while(n%2==0)
+        {
+            n=n/2;
+        } 
+        return n==1;
     }
 }
