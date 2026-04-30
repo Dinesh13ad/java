fe2983e2c69ac612e5ef2bbdfbc4c64da2ad8728
dinesh13ad <720822108016@hit.edu.in>
2026-04-30 22:15:39 +0530

    To find the length of the string

--- length.java
+++ length.java
@@ -1,6 +1,9 @@
+import java.util.*;
 class length{
+
     public static void main(String []args){
-        String s1="Java";
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
         String s2="SQL";
         System.out.println(s1.length());
         System.out.println(s2.length());
