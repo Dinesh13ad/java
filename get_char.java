7f3ba8ead1786b70fbd9141e92fb91d3442574e4
dinesh13ad <720822108016@hit.edu.in>
2026-04-30 22:48:47 +0530

    to get char for the strings

---
+++ get_char.java
@@ -0,0 +1,9 @@
+class get_char{
+    public static void main(String []args){
+        String s1="Java is easy to learn";
+        System.out.println(s1.charAt(2));
+        System.out.println(s1.charAt(5));
+        System.out.println(s1.charAt(6));
+        System.out.println(s1.charAt(4));
+    }
+}
